7692d5b29bb34d8a81430cb3d75a52abd76e466e
Todd Birchard <toddbirchard@gmail.com>
2019-08-09 18:31:36 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Pandas SQLAlchemy Tutorial
+
+![Python](https://img.shields.io/badge/Python-v3.7-blue.svg?logo=python&longCache=true&logoColor=white&colorB=5e81ac&style=flat-square&colorA=4c566a)
+![Pandas](https://img.shields.io/badge/Pandas-v0.25.0-blue.svg?logo=Google&longCache=true&logoColor=white&colorB=5e81ac&style=flat-square&colorA=4c566a)
+![SQLAlchemy](https://img.shields.io/badge/SQLAlchemy-1.3.6-red.svg?longCache=true&style=flat-square&logo=scala&logoColor=white&colorA=4c566a&colorB=bf616a)
+![Psycopg2-Binary](https://img.shields.io/badge/Psycopg2--Binary-v2.7.7-red.svg?longCache=true&style=flat-square&logo=PostgreSQL&logoColor=white&colorA=4c566a&colorB=bf616a)
+![GitHub Last Commit](https://img.shields.io/github/last-commit/google/skia.svg?style=flat-square&colorA=4c566a&colorB=a3be8c)
+[![GitHub Issues](https://img.shields.io/github/issues/toddbirchard/pandas-sqlalchemy-tutorial.svg?style=flat-square&colorA=4c566a&colorB=ebcb8b)](https://github.com/toddbirchard/googlecloud-storage-tutorial/issues)
+[![GitHub Stars](https://img.shields.io/github/stars/toddbirchard/pandas-sqlalchemy-tutorial.svg?style=flat-square&colorB=ebcb8b&colorA=4c566a)](https://github.com/toddbirchard/googlecloud-storage-tutorial/stargazers)
+[![GitHub Forks](https://img.shields.io/github/forks/toddbirchard/pandas-sqlalchemy-tutorial.svg?style=flat-square&colorA=4c566a&colorB=ebcb8b)](https://github.com/toddbirchard/googlecloud-storage-tutorial/network)
---
+++ main.py
@@ -0,0 +1,54 @@
+from os import environ
+from sqlalchemy import create_engine
+from sqlalchemy.types import Integer, Text, String, DateTime
+import pandas as pd
+
+
+db_URI = environ.get('SQLALCHEMY_DATABASE_URI')
+db_schema = environ.get('SQLALCHEMY_DB_SCHEMA')
+engine = create_engine(db_URI,
+                       connect_args={'sslmode': 'require'},
+                       echo=True)
+
+
+def prepare_data():
+    """Create DataFrame and clean column names."""
+    jobs_DF = pd.read_csv('data/nyc-jobs.csv')
+    new_columns = [column.replace(' ', '_').lower() for column in jobs_DF]
+    jobs_DF.columns = new_columns
+    return jobs_DF
+
+
+def upload_dataframe_to_sql(jobs_DF):
+    """Upload data to db with proper dtypes."""
+    jobs_DF.to_sql("nyc_jobs",
+                   engine,
+                   if_exists='append',
+                   schema=db_schema,
+                   index=False,
+                   chunksize=500,
+                   dtype={"job_id": Integer,
+                          "agency": Text,
+                          "business_title": Text,
+                          "job_category":  Text,
+                          "salary_range_from": Integer,
+                          "salary_range_to": Integer,
+                          "salary_frequency": String(50),
+                          "work_location": Text,
+                          "division/work_unit": Text,
+                          "job_description": Text,
+                          "posting_date": DateTime,
+                          "posting_updated": DateTime})
+
+
+def get_dataframe_from_sql(table_name):
+    """Create DataFrame form SQL table."""
+    sql_DF = pd.read_sql_table(table_name,
+                               con=engine,
+                               parse_dates=['posting_date', 'posting_updated'])
+    return sql_DF
+
+
+jobs_DF = prepare_data()
+upload_dataframe_to_sql(jobs_DF)
+get_dataframe_from_sql('nyc_jobs')
